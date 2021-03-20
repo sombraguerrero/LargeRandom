d8e62368786997322fcdb5e4b02e94a65cebc593
Robert Setter <r.setter@comcast.net>
2021-03-20 16:12:33 -0500

    Add project files.

---
+++ LargeRandom/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LargeRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new();
+            long bigNext = LongRandom.NextLong(random);
+            long bigNextMax = LongRandom.NextLong(random, 6000000000);
+            long bigNextRange = LongRandom.NextLong(random, 5000000000, 7000000000);
+            Console.WriteLine($"Random 64-bit integer: {bigNext}{Environment.NewLine}Random 64-bit integer up to 6 billion: {bigNextMax}{Environment.NewLine}Random 64-bit integer between 5 and 7 billion: {bigNextRange}.");
+        }
+    }
+}
